03fbd4470fcf4ddc2f286af160c2d3f5a6b9d65c
extrange <29305375+extrange@users.noreply.github.com>
2023-12-12 12:54:16 +0800

    add sshfs to test

--- hosts/test/system.nix
+++ hosts/test/system.nix
@@ -1,4 +1,37 @@
 { config, specialArgs, pkgs, lib, ... }:
 {
   services.keyd.enable = lib.mkForce false;
+
+  # COPIED FROM LAPTOP
+  fileSystems =
+    let
+      options = {
+        options = [
+          "noauto"
+          "noatime"
+          "user"
+          "_netdev"
+          "allow_other"
+
+          # Fixes sshfs not mounting automatically at boot
+          "x-systemd.automount"
+
+        ];
+        fsType = "fuse.sshfs";
+        noCheck = true; # Disable fsck
+      };
+    in
+    {
+      "/mnt/storage" = {
+        device = "ssh.nicholaslyz.com:/mnt/storage";
+      } // options;
+
+      "/mnt/workspace" = {
+        device = "ssh.nicholaslyz.com:/home/user";
+      } // options;
+    };
+
+  environment.systemPackages = with pkgs; [
+    sshfs # Can't be in user
+  ];
 }
