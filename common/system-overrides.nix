656e7a9a8a228d57a73c94d0acf7f20a6791c93c
extrange <29305375+extrange@users.noreply.github.com>
2024-01-03 09:29:26 +0800

    update flake

--- common/system-overrides.nix
+++ common/system-overrides.nix
@@ -3,5 +3,6 @@
 {
   # Workaround for Obsidian
   # https://github.com/NixOS/nixpkgs/issues/273611
-  nixpkgs.config.permittedInsecurePackages = lib.optional (pkgs.obsidian.version == "1.4.16") "electron-25.9.0";
+  nixpkgs.config.permittedInsecurePackages = lib.optional (pkgs.obsidian.version == "1.5.3") "electron-25.9.0";
 }
+ 
