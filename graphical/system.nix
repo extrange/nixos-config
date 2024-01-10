ae67beab30de9516610f4e9e145c30ddc1eb8767
extrange <29305375+extrange@users.noreply.github.com>
2024-01-10 16:20:28 +0800

    don't mount sshfs on headless servers

--- graphical/system.nix
+++ graphical/system.nix
@@ -37,6 +37,40 @@
     pulse.enable = true;
   };
 
+  # Add SSHFS mounts for server
+  fileSystems =
+    let
+      options = {
+        options = [
+          "noauto"
+          "noatime"
+          "user"
+          "_netdev"
+          "allow_other"
+          "reconnect" # handle connection drops
+          "ServerAliveInterval=15" # keep connections alive
+
+          # Fixes sshfs not mounting automatically at boot
+          "x-systemd.automount"
+
+        ];
+        fsType = "fuse.sshfs";
+        noCheck = true; # Disable fsck
+      };
+    in
+    {
+      "/mnt/storage" = {
+        device = "ssh.nicholaslyz.com:/mnt/storage";
+      } // options;
+
+      "/mnt/workspace" = {
+        device = "ssh.nicholaslyz.com:/home/user";
+      } // options;
+    };
+
+  environment.systemPackages = with pkgs; [
+    sshfs # Can't be in user
+  ];
 
   services.keyd = {
     enable = true;
