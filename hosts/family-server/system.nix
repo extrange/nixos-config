fcf066c1cb133d4043131eab06431b9d68d1471d
extrange <29305375+extrange@users.noreply.github.com>
2024-01-10 12:54:34 +0800

    add server ssh key, wifi for family-server

--- hosts/family-server/system.nix
+++ hosts/family-server/system.nix
@@ -8,18 +8,58 @@
   systemd.services."autovt@tty1".enable = lib.mkForce true;
 
   # No disk encryption
-  boot.initrd.luks.devices = lib.mkForce {};
+  boot.initrd.luks.devices = lib.mkForce { };
 
   # Users allowed to SSH into this served
   users.users."user".openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN3RCwHWzK/gKI8Lplk/qoaoJemh8h/op5Oe7/IXepWK laptop"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINf049gcBU+JxBwkylDpOIGMtk667LfSylzoM1SPZA90 test"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGyJ0LttXH9j3Ql7J1ccJbhLWdYhYn24qR6a8ur72hVi desktop"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAm3fEcDvIM7cFCjB3vzBb4YctOGMpjf8X3IxRl5HhjV server"
   ];
 
   services.openssh = {
     enable = true;
     settings.PasswordAuthentication = false;
     settings.KbdInteractiveAuthentication = false;
+  };
+
+  # Enable wifi temporarily
+
+  boot.extraModulePackages = with config.boot.kernelPackages; [
+    rtl8821cu
+  ];
+
+  sops.secrets = {
+    wifi = { };
+  };
+
+  networking.networkmanager.ensureProfiles = {
+    environmentFiles = [ config.sops.secrets.wifi.path ];
+    profiles = {
+      home-wifi = {
+        connection = {
+          id = "home-wifi";
+          type = "wifi";
+          interface-name = "wlp0s29u1u4i2";
+        };
+        wifi = {
+          mode = "infrastructure";
+          ssid = "$home_wifi_ssid";
+        };
+        wifi-security = {
+          auth-alg = "open";
+          key-mgmt = "wpa-psk";
+          psk = "$home_wifi_psk";
+        };
+        ipv4 = {
+          method = "auto";
+        };
+        ipv6 = {
+          addr-gen-mode = "default";
+          method = "auto";
+        };
+      };
+    };
   };
 }
