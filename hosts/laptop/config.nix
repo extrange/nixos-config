c9a93e1c772b0fd59d9652f58a70b1965131b6da
extrange <29305375+extrange@users.noreply.github.com>
2024-01-27 22:39:21 +1100

    add laptop automatic location service

--- hosts/laptop/config.nix
+++ hosts/laptop/config.nix
@@ -7,6 +7,9 @@
     interface-name = "wlp1s0";
   };
 
+  # Allow TZ to be set automatically
+  time.timeZone = lib.mkForce null;
+
   hardware.opengl.extraPackages = with pkgs; [
     intel-media-driver
     intel-compute-runtime
@@ -20,6 +23,10 @@
       "org/gnome/desktop/peripherals/touchpad" = {
         tap-to-click = true;
       };
+
+      # Auto TZ
+      "org/gnome/desktop/datetime" = { automatic-timezone = true; };
+      "org/gnome/system/location" = { enabled = true; };
     };
   };
 }
