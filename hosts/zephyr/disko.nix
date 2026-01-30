652fa856c4e90f6bb1ebd921bf5cadd9422e3d81
extrange <29305375+extrange@users.noreply.github.com>
2026-01-30 14:23:26 +0800

    feat(zephyr): add disko config

---
+++ hosts/zephyr/disko.nix
@@ -0,0 +1,45 @@
+{
+  disko.devices = {
+    disk = {
+      main = {
+        type = "disk";
+        device = "/dev/disk/by-id/nvme-WDC_PC_SN520_SDAPNUW-128G-1006_1903C4801837"; # 128GB NVME
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              priority = 1;
+              name = "ESP";
+              start = "1M";
+              end = "1024M";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [ "umask=0077" ];
+              };
+            };
+            root = {
+              size = "100%";
+              content = {
+                type = "btrfs";
+                subvolumes = {
+                  "/root" = {
+                    mountOptions = [
+                      "noatime"
+                      "compress=zstd"
+                    ];
+                    mountpoint = "/";
+                  };
+                };
+                mountpoint = "/mnt/partition-root";
+              };
+              swap.swapfile.size = "8G";
+            };
+          };
+        };
+      };
+    };
+  };
+}
