dab9f8ab75f7ff776085bf45caf47dc45d0f6e99
extrange <29305375+extrange@users.noreply.github.com>
2026-01-31 23:11:22 +0800

    feat: add zephyr to authorizedkeys

--- common-opt/allowSsh.nix
+++ common-opt/allowSsh.nix
@@ -10,11 +10,12 @@ with lib;
     let
       cfg = config.allowSsh;
       # These are added to /etc/ssh/authorized_keys.d
-      authorizedKeys = [
+      authorizedDeviceKeys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN3RCwHWzK/gKI8Lplk/qoaoJemh8h/op5Oe7/IXepWK laptop"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINf049gcBU+JxBwkylDpOIGMtk667LfSylzoM1SPZA90 test"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGyJ0LttXH9j3Ql7J1ccJbhLWdYhYn24qR6a8ur72hVi desktop"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEZXrm0AXgoOcJWckgr/ZgYVdHKrJHJg5G52bIx6zc4b server"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOreujIuA7XmkluU/U8r2Zjjx+Mv1nprYEFXRLj1rwM5 user@zephyr"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBy46svGzZn4VfcAb3+kp2/5dxvpk3IKzLh1Kn4YCzCb chanel@server"
 
       ];
@@ -27,7 +28,7 @@ with lib;
           ClientAliveInterval = 15;
         };
       };
-      users.users."user".openssh.authorizedKeys.keys = authorizedKeys;
-      users.users."root".openssh.authorizedKeys.keys = mkIf cfg.forRoot authorizedKeys;
+      users.users."user".openssh.authorizedKeys.keys = authorizedDeviceKeys;
+      users.users."root".openssh.authorizedKeys.keys = mkIf cfg.forRoot authorizedDeviceKeys;
     };
 }
