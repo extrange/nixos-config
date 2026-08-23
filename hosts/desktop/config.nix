d6198aec9067d3a9743c8ed41c714e72f80ed520
extrange <29305375+extrange@users.noreply.github.com>
2026-08-23 18:26:00 +0800

    refactor(disko): add disk configuration for NVMe drive and remove obsolete monitor settings

--- hosts/desktop/config.nix
+++ hosts/desktop/config.nix
@@ -21,15 +21,6 @@
     "dialout" # For ESP32 programming
   ];
 
-  # Boot drive encryption
-  boot.initrd.luks.devices."luks-primary" = {
-    device = "/dev/disk/by-label/primary";
-    bypassWorkqueues = true; # https://nicholaslyz.com/blog/2025/05/14/dm-crypt-causing-system-freezes/
-  };
-
-  # Allow this host to redirect its USB devices to VMs
-  virtualisation.spiceUSBRedirection.enable = true;
-
   home-manager.users.user = {
     home.packages = with pkgs; [
       clinfo # Check OpenCL
@@ -38,43 +29,24 @@
       nvtopPackages.amd
     ];
 
-    dconf.settings =
-      with home-manager.lib.hm.gvariant;
-      let
-        qemuUris = [ "qemu+ssh://root@zephyr/system" ];
-      in
-      {
-        # Virt-manager connections
-        "org/virt-manager/virt-manager/connections" = {
-          uris = qemuUris;
-        };
-        "org/virt-manager/virt-manager/connections" = {
-          autoconnect = qemuUris;
-        };
-        "org/gnome/mutter" = {
-          # Fractional scaling
-          experimental-features = [ "scale-monitor-framebuffer" ];
-        };
-        "org/gnome/desktop/session" = {
-          idle-delay = mkUint32 900; # 15mins
-        };
-
-        "org/gnome/shell/extensions/vitals" = {
-          hot-sensors = [
-            "_processor_usage_"
-            "_memory_usage_"
-            "_temperature_processor_0_"
-            "__network-rx_max__"
-            "_temperature_amdgpu_edge_"
-          ];
-        };
+    dconf.settings = with home-manager.lib.hm.gvariant; {
+      "org/gnome/mutter" = {
+        # Fractional scaling
+        experimental-features = [ "scale-monitor-framebuffer" ];
+      };
+      "org/gnome/desktop/session" = {
+        idle-delay = mkUint32 900; # 15mins
       };
 
-    # Set fractional scaling and monitor position
-    home.file.".config/monitors.xml" = {
-      source = ./monitors.xml;
-      force = true; # overwrite existing
+      "org/gnome/shell/extensions/vitals" = {
+        hot-sensors = [
+          "_processor_usage_"
+          "_memory_usage_"
+          "_temperature_processor_0_"
+          "__network-rx_max__"
+          "_temperature_amdgpu_edge_"
+        ];
+      };
     };
-
   };
 }
