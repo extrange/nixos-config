589cb2006b93318359f35079e96586c0a25b2e86
extrange <29305375+extrange@users.noreply.github.com>
2026-08-23 20:58:22 +0800

    refactor(disko): update disk device path to use by-id reference

--- hosts/desktop/disko.nix
+++ hosts/desktop/disko.nix
@@ -3,7 +3,7 @@
     disk = {
       main = {
         type = "disk";
-        device = "/dev/nvme-SAMSUNG_MZVLB512HBJQ-00000_S4GENX0N733466";
+        device = "/dev/disk/by-id/nvme-SAMSUNG_MZVLB512HBJQ-00000_S4GENX0N733466";
         content = {
           type = "gpt";
           partitions = {
